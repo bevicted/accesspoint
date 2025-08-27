fa759fa1aed0656e0f8e54ad67ff4dec5ec9555d
bevicted <170766675+bevicted@users.noreply.github.com>
2025-08-27 09:16:31 +0200

    fix panic
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- src/catalog.rs
+++ src/catalog.rs
@@ -11,11 +11,18 @@ const TEMPLATE_STRING: &str = "STRING";
 const TEMPLATE_NUMBER: &str = "NUMBER";
 const RESERVED_KEYS: &[&str] = &[TEMPLATE_STRING, TEMPLATE_NUMBER];
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
+enum TemplateType {
+    Reference,
+    Input,
+}
+
+#[derive(Debug, Clone)]
 struct Template {
     var: String,
     start: usize,
     end: usize,
+    t: TemplateType,
 }
 
 pub fn parse(path: &PathBuf) -> Result<HashMap<String, Table>> {
@@ -24,7 +31,7 @@ pub fn parse(path: &PathBuf) -> Result<HashMap<String, Table>> {
 
     for (project, fields) in table.iter() {
         dbg!(project);
-        let mut templates = parse_templates(fields);
+        let templates = parse_templates(fields)?;
 
         for k in templates.keys() {
             let mut visit_tree = BTreeSet::new();
@@ -32,27 +39,31 @@ pub fn parse(path: &PathBuf) -> Result<HashMap<String, Table>> {
 
             while let Some(field) = queue.pop_front() {
                 if visit_tree.contains(field) {
-                    return Err(format!("cyclic import: {} <-> {}", k, field).into());
+                    return Err(format!("cyclic import: {} <-> {}", *k, field).into());
                 }
                 visit_tree.insert(field);
 
                 if templates.contains_key(field) {
-                    for k in &templates[field] {
-                        let v = &fields[&k.var];
+                    for template in &templates[field] {
+                        if matches!(template.t, TemplateType::Reference) {
+                            continue;
+                        }
+                        dbg!(&template.var);
+                        let v = &fields[&template.var];
                         if !(v.is_str() || v.is_integer() || v.is_float() || v.is_bool()) {
                             return Err(format!(
                                 "cant template {} of type {}",
-                                k.var,
+                                template.var,
                                 v.type_str()
                             )
                             .into());
                         }
-                        queue.push_back(&k.var);
+                        queue.push_back(&template.var);
                     }
                 }
             }
 
-            resolve(k, &mut fields, &mut templates);
+            //resolve(k, &mut fields, &mut templates);
         }
     }
     Ok(table)
@@ -62,7 +73,7 @@ fn parse_templates(fields: &Table) -> Result<HashMap<&String, Vec<Template>>> {
     let mut templates = HashMap::new();
     for (field_key, field_val) in fields.iter() {
         dbg!(field_key);
-        let mut field_deps = Vec::new();
+        let mut field_templates = Vec::new();
         if RESERVED_KEYS.contains(&field_key.as_str()) {
             return Err("used reserved variable name".into());
         }
@@ -75,23 +86,31 @@ fn parse_templates(fields: &Table) -> Result<HashMap<&String, Vec<Template>>> {
         for (i, c) in s.char_indices() {
             if !in_var && c == '{' {
                 in_var = true;
-                var_start = i;
+                var_start = i + 1;
                 continue;
             }
             if in_var && c == '}' {
                 in_var = false;
-                field_deps.push(Template {
-                    var: s[var_start..i + 1].to_owned(),
+                let var = &s[var_start..i];
+                let template_type = if RESERVED_KEYS.contains(&var) {
+                    TemplateType::Reference
+                } else {
+                    TemplateType::Input
+                };
+                field_templates.push(Template {
+                    var: var.to_owned(),
                     start: var_start,
                     end: i + 1,
+                    t: template_type,
                 });
             }
         }
-        if !field_deps.is_empty() {
-            templates.insert(field_key, field_deps);
+        if !field_templates.is_empty() {
+            templates.insert(field_key, field_templates);
         }
     }
-    return Ok(templates);
+
+    Ok(templates)
 }
 
 fn resolve(field: &String, fields: &mut Table, templates: &mut HashMap<&String, Vec<Template>>) {
@@ -101,11 +120,11 @@ fn resolve(field: &String, fields: &mut Table, templates: &mut HashMap<&String, 
 
     let unresolved_val = fields[field].as_str().unwrap().to_owned();
     let mut resolved_val = String::new();
-    let mut start = 0;
+    let mut from = 0;
 
     for template in templates.remove(field).unwrap() {
         resolve(&template.var, fields, templates);
-        resolved_val.push_str(&unresolved_val[start..template.start]);
+        resolved_val.push_str(&unresolved_val[from..template.start]);
         let template_val = match &fields[&template.var] {
             toml::Value::String(v) => v,
             toml::Value::Integer(v) => &v.to_string(),
@@ -114,9 +133,9 @@ fn resolve(field: &String, fields: &mut Table, templates: &mut HashMap<&String, 
             _ => unreachable!(),
         };
         resolved_val.push_str(template_val);
-        start = template.end;
+        from = template.end;
     }
 
-    resolved_val.push_str(&unresolved_val[start..]);
+    resolved_val.push_str(&unresolved_val[from..]);
     fields[field] = resolved_val.into();
 }
