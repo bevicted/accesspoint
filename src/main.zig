200f56758ad2a7dfdc2a8cdbf61e28f3140c83be
bevicted <170766675+bevicted@users.noreply.github.com>
2025-10-25 18:40:27 +0200

    alloc for line
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,11 @@ const accesspoint = @import("accesspoint");
 const DELIMITER = if (builtin.os.tag == .windows) '\r' else '\n';
 
 pub fn main() !void {
-    // const gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
-    // defer if (gpa.deinit() == .leak) {
-    //     std.log.err("Memory leak", .{});
-    // };
-    // const allocator = gpa.allocator();
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    defer if (gpa.deinit() == .leak) {
+        std.log.err("Memory leak", .{});
+    };
+    const allocator = gpa.allocator();
 
     const path = "test.ap"; // get from CLI later
     const file = std.fs.cwd().openFile(path, .{ .mode = .read_only }) catch |err| {
@@ -17,21 +17,20 @@ pub fn main() !void {
     };
     defer file.close();
 
-    var w_buf: [4096]u8 = undefined;
-    var w: std.io.Writer = .fixed(&w_buf);
+    var line_allocator: std.io.Writer.Allocating = .init(allocator);
+    defer line_allocator.deinit();
+
     var r_buf: [4096]u8 = undefined;
     var fr = file.reader(&r_buf);
     const r = &fr.interface;
 
     while (true) {
-        _ = r.streamDelimiter(&w, DELIMITER) catch |err| switch (err) {
-            error.EndOfStream => break,
-            else => return err,
+        _ = r.streamDelimiter(&line_allocator.writer, DELIMITER) catch |err| {
+            if (err == error.EndOfStream) break else return err;
         };
         r.toss(1);
-        try w.flush();
-        std.debug.print("{s}\n", .{w.buffered()});
-        w.end = 0;
+        std.debug.print("{s}\n", .{line_allocator.written()});
+        line_allocator.clearRetainingCapacity();
     }
 
     try accesspoint.bufferedPrint();
