c9fe8a4d5074c3d57cabe4972a0c591e89d7fda5
bevicted <170766675+bevicted@users.noreply.github.com>
2025-10-27 20:57:02 +0100

    split fn, use structs
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,21 @@ const std = @import("std");
 const builtin = @import("builtin");
 const accesspoint = @import("accesspoint");
 
+const Allocator = std.mem.Allocator;
+
 const DELIMITER = if (builtin.os.tag == .windows) '\r' else '\n';
+
+const Entries = struct {
+    allocator: *const Allocator,
+    parsed: []std.json.Parsed(Entry),
+
+    pub fn deinit(self: @This()) void {
+        for (self.parsed) |p| {
+            p.deinit();
+        }
+        self.allocator.free(self.parsed);
+    }
+};
 
 const Entry = struct {
     name: ?[]u8 = null,
@@ -20,10 +34,26 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     const path = "test.ap"; // get from CLI later
+    const entries = try parseFile(allocator, path);
+    defer entries.deinit();
+
+    for (entries.parsed) |p| {
+        if (p.value.name) |name| {
+            std.debug.print("{s}\n", .{name});
+        }
+    }
+
+    try accesspoint.bufferedPrint();
+}
+
+fn parseFile(allocator: Allocator, path: []const u8) !Entries {
     const file = std.fs.cwd().openFile(path, .{ .mode = .read_only }) catch |err| {
         return err;
     };
     defer file.close();
+
+    var entries: std.ArrayList(std.json.Parsed(Entry)) = .empty;
+    defer entries.deinit(allocator);
 
     var line_allocator: std.io.Writer.Allocating = .init(allocator);
     defer line_allocator.deinit();
@@ -31,7 +61,6 @@ pub fn main() !void {
     var r_buf: [4096]u8 = undefined;
     var fr: std.fs.File.Reader = file.reader(&r_buf);
     const r: *std.io.Reader = &fr.interface;
-    //var entries: []Entry = undefined;
 
     while (true) {
         _ = r.streamDelimiter(&line_allocator.writer, DELIMITER) catch |err| {
@@ -42,20 +71,21 @@ pub fn main() !void {
         line_allocator.clearRetainingCapacity();
 
         std.debug.print("{s}\n", .{line});
-        var indent: usize = undefined;
-        for (0.., line) |idx, char| {
-            if (char != ' ') {
-                indent = idx;
-                break;
+        var indent: usize = 0;
+        for (line) |char| {
+            switch (char) {
+                ' ' => indent += 1,
+                '\t' => indent += 4,
+                else => break,
             }
         }
         const parsed: std.json.Parsed(Entry) = try std.json.parseFromSlice(Entry, allocator, line[indent..], .{});
-        const entry = parsed.value;
-        if (entry.name) |name| {
-            std.debug.print("{s}\n", .{name});
-        }
-        parsed.deinit();
+        errdefer parsed.deinit();
+        try entries.append(allocator, parsed);
     }
 
-    try accesspoint.bufferedPrint();
+    return Entries{
+        .allocator = &allocator,
+        .parsed = try entries.toOwnedSlice(allocator),
+    };
 }
