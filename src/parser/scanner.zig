038e5e4444c1269f1e84809833d446c7001bc790
bevicted <170766675+bevicted@users.noreply.github.com>
2026-02-17 20:49:33 +0100

    WIP: identifier
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- src/parser/scanner.zig
+++ src/parser/scanner.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 const Token = @import("token.zig");
 
-const assert = std.debug.assert;
-
 const Scanner = struct {
     source: []const u8,
-    current_idx: u64,
+    current_token_start: usize,
+    current_token_end: usize,
     line: usize,
 
     const Self = @This();
@@ -13,40 +12,41 @@ const Scanner = struct {
     pub fn init(source: []const u8) Scanner {
         return Scanner{
             .source = source,
-            .current_idx = 0,
+            .current_token_start = 0,
+            .current_token_end = 0,
             .line = 1,
         };
     }
 
     fn is_at_end(self: Self) bool {
-        return self.current_idx >= self.source.len;
+        return self.current_token_end >= self.source.len;
     }
 
     fn advance(self: Self) void {
-        self.current_idx += 1;
+        self.current_token_end += 1;
     }
 
     fn get_current_char(self: Self) ?u8 {
         if (self.is_at_end()) return null;
-        return self.source[self.current_idx];
+        return self.source[self.current_token_end];
     }
 
     fn get_next_char(self: Self) ?u8 {
-        if (self.current_idx + 1 >= self.source.len) return null;
-        return self.source[self.current_idx + 1];
+        if (self.current_token_end + 1 >= self.source.len) return null;
+        return self.source[self.current_token_end + 1];
     }
 
     fn match_char(self: Self, target_char: u8) bool {
         if (self.is_at_end()) return false;
         if (self.get_current_char() != target_char) return false;
-        self.current_idx += 1;
+        self.current_token_end += 1;
         return true;
     }
 
     fn make_token(self: Self, kind: Token.Kind) Token {
         return .{
             .kind = kind,
-            .lexeme = self.source[0..self.current_idx],
+            .lexeme = self.source[self.current_token_start..self.current_token_end],
             .line = self.line,
         };
     }
@@ -85,15 +85,32 @@ const Scanner = struct {
         }
     }
 
-    fn check_keyword(self: Self, offset: usize, str: []const u8, kind: Token.Kind) Token.Kind {
-        if (self.current_idx != str.len + offset) return .IDENTIFIER;
-        const source_slice = self.source[offset..self.current_idx];
-        assert(source_slice.len == str.len);
-        return if (std.mem.eql(u8, source_slice, str)) kind else .IDENTIFIER;
+    fn match_keyword(self: Self, offset: usize, target: []const u8, target_kind: Token.Kind) Token.Kind {
+        std.debug.assert(target.len > offset);
+        const source_slice = self.source[self.current_token_start + offset .. target.len - offset];
+        return if (std.mem.eql(u8, source_slice, target[offset..])) target_kind else .IDENTIFIER;
+    }
+
+    fn match_identifier(self: Self) Token.Kind {
+        return switch (self.source[self.current_token_start]) {
+            'l' => switch (self.source[self.current_token_start + 1]) {
+                'a' => self.match_keyword(2, "layer", .LAYER),
+                'e' => self.match_keyword(2, "let", .LET),
+            },
+            'o' => self.match_keyword(1, "open", .OPEN),
+            'p' => self.match_keyword(1, "print", .PRINT),
+            'r' => self.match_keyword(1, "run", .RUN),
+            else => .IDENTIFIER,
+        };
+    }
+
+    fn consume_identifier(self: Self) Token {
+        while (is_alpha_numeric(self.get_current_char())) self.advance();
+        return self.make_token(self.match_identifier());
     }
 };
 
-fn is_aplha(c: u8) bool {
+fn is_alpha(c: u8) bool {
     return (c >= 'a' and c <= 'z') or
         (c >= 'A' and c <= 'Z') or
         c == '_';
@@ -101,4 +118,8 @@ fn is_aplha(c: u8) bool {
 
 fn is_digit(c: u8) bool {
     return c >= '0' and c <= '9';
+}
+
+fn is_alpha_numeric(c: u8) bool {
+    return is_alpha(c) or is_digit(c);
 }
